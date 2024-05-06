610a18f891f336f970b0420ad0d901eab81f6450
David Ulrich <david.hau1@gmail.com>
2024-05-06 16:54:15 +0200

    updating

--- main.c
+++ main.c
@@ -6,51 +6,70 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 11:41:33 by dulrich           #+#    #+#             */
-/*   Updated: 2024/03/13 14:53:44 by dulrich          ###   ########.fr       */
+/*   Updated: 2024/05/06 16:52:25 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	create_philos(t_philos philo)
+void	create_philos(t_philo philos)
 {
 	int	i;
 	pthread_t	var;
 
-	i = 0;
-	while (i < philos)
+	i = 1;
+	while (i <= philos.nbr_of_philos)
 	{
-		
+		/*create philo-threads
+		pthread_create 
+		*/
 	}
 }
 
-int	ft_atoi(char *s)
+void	is_eating()
 {
-	int	i;
-	int	nbr;
-	int	sign;
-	
-	i = 0;
-	nbr = 0;
-	sign = 1;
-	while (s[i] >= 9 && s[i] <= 13 || s[i] == 32)
-		i++;
-	if (s[i] == '-')
-		sign = -1;
-	if (s[i] == '-' || s[i] == '+')
-		i++;
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		nbr *= 10;
-		nbr += s[i++] - 48;
-	}
-	return (nbr * sign);
+	/* 
+	if philo n is eating, philo n+1 and philo n-1 can't
+	have a waiter who checks who is currently eating
+	 */
 }
 
-void	init_program(t_philo philo)
+void	init_program(t_philo philos, char *nbr_of_philos)
 {
-	philo.is_dead_flag = FALSE;
+	philos.is_dead_flag = FALSE;
+	philos.nbr_of_philos = ft_atoi(nbr_of_philos);
+	pthread_mutex_init(&)
+}
+
+void	ft_usleep()
+{
 	
+}
+
+void	state_change()
+{
+	
+}
+
+void	philo_loop(t_data philos)
+{
+	while(!philos.is_dead_flag)
+		run_philo_routine();
+		if (all_meals_finished)
+			return ;
+	if (philos.is_dead_flag)
+		return (printf())
+}
+
+void	thinking(t_philo philos)
+{
+	printf("%d is thinking", philos.id);
+}
+
+void	sleeping(t_philo philos)
+{
+	// use ft_usleep(philos.) to let them sleep for the amount of time inputted
+	printf("%d is sleeping", philos.id);
 }
 
 int	check_arg_nbrs(char *str)
@@ -66,44 +85,43 @@ int	check_arg_nbrs(char *str)
 	}
 	return (0);
 }
-
-void	ft_usleep()
-{
-	
-}
-
-void	philo_loop(t_philo philo)
-{
-	while(!philo.is_dead_flag)
-		run_philo_routine();
-		if (all_meals_finished)
-			return ;
-}
-
-int	check_args(char **argv)
+int	check_args(char **argv, t_philo philos)
 {
 	if (ft_atoi(argv[1]) > MAX_PHILOS || ft_atoi(argv[1]) <= 0 || \
 		check_arg_nbrs(argv[1]) == 1)
 		return (printf("Invalid number of philosophers."), 1);
+	else
+		philos.nbr_of_philos = ft_atoi(argv[1]);
 	if (ft_atoi(argv[2]) <= 0 || check_arg_nbrs(argv[2]) == 1)
 		return (printf("Invalid time to die."), 1);
+	else
+		philos.time_to_die = ft_atoi(argv[2]);
 	if (ft_atoi(argv[3]) <= 0 || check_arg_nbrs(argv[3]) == 1)
 		return (printf("Invalid time to eat."), 1);
+	else
+		philos.time_to_eat = ft_atoi(argv[3]);
 	if (ft_atoi(argv[4]) <= 0 || check_arg_nbrs(argv[4]) == 1)
 		return (printf("Invalid time to sleep."), 1);
+	else
+		philos.time_to_sleep = ft_atoi(argv[4]);
 	if (argv[5] && (ft_atoi(argv[5]) <= 0 || check_arg_nbrs(argv[5]) == 1))
 		return (printf("Invalid nbr of times each philosopher must eat."), 1);
+	else if (argv[5] && !(ft_atoi(argv[5]) <= 0 || check_arg_nbrs(argv[5]) == 1))
+		philos.max_nbr_of_meals = ft_atoi(argv[5]);
 }
 
 int	main(int argc, char **argv)
 {
-	t_philo	philo;
+	t_philo	philos;
+	t_data	data;
+	pthread_mutex_t forks[MAX_PHILOS];
 	
 	if (argc != 5 || argc != 6)
 		return (1);
-	if (check_args(argv) == 1)
+	if (check_args(argv, philos) == 1)
 		return (1);
-	init_program(philo);
-	create_philos(philo);
-	
+	init_program(philos, argv[1]);
+	create_philos(philos);
+	philo_loop(data);
+	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 11:42:18 by dulrich           #+#    #+#             */
-/*   Updated: 2024/03/13 14:54:57 by dulrich          ###   ########.fr       */
+/*   Updated: 2024/05/06 16:35:16 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,29 @@
 
 typedef struct s_philo
 {
-	int		is_dead_flag;
+	int		id;
+	int		meals_eaten;
+	size_t	nbr_of_philos;
 	size_t	time_to_die;
 	size_t	time_to_eat;
 	size_t	time_to_sleep;
 	size_t	max_nbr_of_meals;
-	
+	size_t	r_fork;
+	size_t	l_fork;	
 }				t_philo;
 
+typedef struct s_data
+{
+	int				is_dead_flag;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	t_philo			*philos;
+}				t_data;
 
 //Functions
 
+
+//Utils
+int	ft_atoi(char *s);
 
 #endif
